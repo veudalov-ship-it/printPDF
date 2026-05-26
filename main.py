ce8fedc465f444fa81644f21870d22dc8b06e5cb
veudalov-ship-it <v.e.udalov@gmail.com>
2026-05-26 13:35:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ from time import sleep
 path = sys.argv[1]
 currentprinter = sys.argv[2]
 
-r = re.search('БланкВозвратаРеалициалицияТоваров', path)
-if not r is None:
-    orientation = '-portrait'
-else:
-    orientation = '-landscape'
-
-
 if getattr(sys, 'frozen', False):
     GHOSTSCRIPT_PATH = os.path.join(sys._MEIPASS, "gs/gs10.02.1/bin/gswin32.exe")
     GSPRINT_PATH = os.path.join(sys._MEIPASS, "GSPRINT/gsprint.exe")
